9d5574a3daa5756e11ba8a2afda4553c76b74c09
AlperenNoldu <alperen.adatepe@titanom.com>
2024-03-08 02:46:00 +0100

    SaaS finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="grid  justify-center mt-10 ">
+      <div className="grid  justify-center items-center mt-10 ">
         <h1 className="text-3xl underline uppercase">
           Video Hosting Platfrom Test
         </h1>
-        <ul className="text-left  list-inside ml-5 mt-5 *:my-5">
+
+        <h2 className="text-2xl mt-5 text-center underline">(SaaS)</h2>
+        <ul className="text-left  list-inside ml-5  *:my-5">
           <li>
             <Link href="/Wistia" className="hover:text-blue-500">
               Wistia
@@ -23,87 +25,43 @@ export default function Home() {
               VdoCipher
             </Link>
           </li>
-          <li>
-            <Link href="/Gumlet" className="hover:text-blue-500">
-              Gumlet
-            </Link>
-          </li>
-          <li>
-            <Link href="/Muvi" className="hover:text-blue-500">
-              Muvi
-            </Link>
-          </li>
-          <li>
-            <Link href="/Cincopa" className="hover:text-blue-500">
-              Cincopa
-            </Link>
-          </li>
-          <li>
-            <Link href="/Vimeo" className="hover:text-blue-500">
-              Vimeo
-            </Link>
-          </li>
-          <li>
-            <Link href="/Dailymotion" className="hover:text-blue-500">
-              Dailymotion
-            </Link>
-          </li>
-          <li>
-            <Link href="/HippoVideo" className="hover:text-blue-500">
-              HippoVideo
-            </Link>
-          </li>
-          <li>
-            <Link href="/QuickaPageInc" className="hover:text-blue-500">
-              QuickaPage Inc.
-            </Link>
-          </li>
-          <li>
-            <Link href="/LoomInc" className="hover:text-blue-500">
-              Loom, Inc.
-            </Link>
-          </li>
-          <li>
-            <Link href="/Brightcove" className="hover:text-blue-500">
-              Brightcove
-            </Link>
-          </li>
-          <li>
-            <Link href="/Uscreen" className="hover:text-blue-500">
-              Uscreen
-            </Link>
-          </li>
-          <li>
-            <Link href="/Dacast" className="hover:text-blue-500">
-              Dacast
-            </Link>
-          </li>
-          <li>
-            <Link href="/Kaltura" className="hover:text-blue-500">
-              Kaltura
-            </Link>
-          </li>
-          <li>
-            <Link href="/SproutVideo" className="hover:text-blue-500">
-              SproutVideo
-            </Link>
-          </li>
-          <li>
-            <Link href="/AmazonS3Favorit" className="hover:text-blue-500">
-              Amazon S3 → Favorit
-            </Link>
-          </li>
-          <li>
-            <Link href="/Brightcove" className="hover:text-blue-500">
-              Brightcove
-            </Link>
-          </li>
-          <li>
-            <Link href="/Moodle" className="hover:text-blue-500">
-              Moodle
-            </Link>
-          </li>
         </ul>
+
+      {/*   <div>
+          <h2 className="text-2xl pt-10 text-center underline">(Self-Host)</h2>
+          <ul className="text-left  list-inside ml-5  *:my-5">
+            <li>
+              <a href="/Kaltura" className="hover:text-blue-500">
+                Kaltura
+              </a>
+            </li>
+            <li>
+              <a href="/MediaDrop" className="hover:text-blue-500">
+                MediaDrop
+              </a>
+            </li>
+            <li>
+              <a href="/PeerTube" className="hover:text-blue-500">
+                PeerTube
+              </a>
+            </li>
+            <li>
+              <a href="/YouPHPTube" className="hover:text-blue-500">
+                YouPHPTube (auch bekannt als AVideo)
+              </a>
+            </li>
+            <li>
+              <a href="/ClipBucket" className="hover:text-blue-500">
+                ClipBucket
+              </a>
+            </li>
+            <li>
+              <a href="/Plumi" className="hover:text-blue-500">
+                Plumi
+              </a>
+            </li>
+          </ul>
+        </div> */}
       </div>
     </>
   );
